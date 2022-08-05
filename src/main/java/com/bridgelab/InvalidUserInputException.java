2acefa644100f7d14fa88390a18c6cebbf87e789
deepak-patil27 <mr.deepak.patil@outlook.com>
2022-08-05 11:46:08 +0530

    Refactored: Throw Custom Exceptions In case Of Invalid User Details

---
+++ src/main/java/com/bridgelab/InvalidUserInputException.java
@@ -0,0 +1,8 @@
+package com.bridgelab;
+
+public class InvalidUserInputException extends Exception {
+	public InvalidUserInputException() {
+		
+	}
+
+}
--- src/main/java/com/bridgelab/UserRegistration.java
+++ src/main/java/com/bridgelab/UserRegistration.java
@@ -1,57 +1,86 @@
 package com.bridgelab;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+	static Scanner scan = new Scanner(System.in);
 
-	public boolean ValidateFirstName(String firstName) {
-		boolean Validation = firstName.matches("[A-Z]{1}[a-z]{2,}");
-		if (Validation) {
-			System.out.println(firstName + " is an Valid First Name");
-		} else {
-			System.out.println(firstName + " is an Invalid First Name");
+	public boolean FirstName() throws InvalidUserInputException {
+		System.out.println("Enter First Name : ");
+		String name = scan.next();
+		String regex = "[A-Z]{1}[a-z]{2,}";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher ma = pattern.matcher(name);
+
+		if (ma.matches() == true) {
+			System.out.println("Entered First Name : " + name + " is " + ma.matches());
+			return ma.matches();
 		}
-		return Validation;
+		// if not match throw exception
+		throw new InvalidUserInputException();
 	}
 
-	public boolean ValidateLastName(String lastName) {
-		boolean Validation = lastName.matches("[A-Z]{1}[a-z]{2,}");
-		if (Validation) {
-			System.out.println(lastName + " is an Valid Last Name");
-		} else {
-			System.out.println(lastName + " is an Invalid Last Name");
+	public boolean LastName() throws InvalidUserInputException {
+		System.out.println("Enter Last Name : ");
+		String lastname = scan.next();
+		String regex = "[A-Z]{1}[a-z]{2,}";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher ma = pattern.matcher(lastname);
+
+		if (ma.matches() == true) {
+			System.out.println("Entered Last Name : " + lastname + " is " + ma.matches());
+			return ma.matches();
 		}
-		return Validation;
+		// if not match throw exception
+		throw new InvalidUserInputException();
 	}
 
-	public static boolean ValidateEmail(String email) {
-		boolean Validation = email
-				.matches("^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$");
-		if (Validation) {
-			System.out.println(email + " is a Valid Email address\n");
-		} else {
-			System.out.println(email + " is an Invalid Email address");
+	public static boolean Email() throws InvalidUserInputException {
+		System.out.println("Enter Email ID: ");
+		String email = scan.next();
+		String regex = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher ma = pattern.matcher(email);
+
+		if (ma.matches() == true) {
+			System.out.println("Entered Email Id : " + email + " is " + ma.matches());
+			return ma.matches();
 		}
-		return Validation;
+		// if not match throw exception
+		throw new InvalidUserInputException();
 	}
 
-	public boolean ValidateMobileNumber(String mobileNumber) {
-		boolean Validation = mobileNumber.matches("^[0-9]{2}\\s{1}[0-9]{10}$");
-		if (Validation) {
-			System.out.println(mobileNumber + " is a Valid  Mobile Number\n");
-		} else {
-			System.out.println(mobileNumber + " is an Invalid Mobile Number");
+	public boolean MobileNumber() throws InvalidUserInputException {
+		System.out.println("Enter Mobile Number : ");
+		String mobile = scan.next();
+		String regex = "(91)?[7-9][0-9]{9}";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher ma = pattern.matcher(mobile);
+
+		if (ma.matches() == true) {
+			System.out.println("Entered Mobile Number : " + mobile + " is " + ma.matches());
+			return ma.matches();
 		}
-		return Validation;
+		// if not match throw exception
+		throw new InvalidUserInputException();
 
 	}
 
-	public boolean ValidatePassword(String Password) {
-		boolean Validation = Password.matches("^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()])([a-zA-Z0-9]*).{8,}$");
-		if (Validation) {
-			System.out.println(Password + " is a Valid  Password\n");
-		} else {
-			System.out.println(Password + " is an Invalid Password");
+	public boolean Password() throws InvalidUserInputException {
+		System.out.println("Enter Password : ");
+		String password = scan.next();
+		String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&-+=()])([a-zA-Z0-9]*).{8,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher ma = pattern.matcher(password);
+
+		if (ma.matches() == true) {
+			System.out.println("Entered Password : " + password + " is " + ma.matches());
+			return ma.matches();
 		}
-		return Validation;
+		// if not match throw exception
+		throw new InvalidUserInputException();
 	}
 
 }
